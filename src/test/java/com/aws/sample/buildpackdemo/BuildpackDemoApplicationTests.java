28ee4b649456401313aa7e510580b5b6e0d12da6
Hari Ohm Prasath Rajagopal <harrajag@amazon.com>
2022-06-01 00:23:22 -0700

    Update repo contents

---
+++ src/test/java/com/aws/sample/buildpackdemo/BuildpackDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aws.sample.buildpackdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildpackDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
